9d316a7e669bb16188b988663a6f491a388d2691
milchevyordan <milchevyordan2@gmail.com>
2025-01-06 19:19:59 +0200

    Add users create

---
+++ resources/js/Pages/Users/Create.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import { Head, useForm } from '@inertiajs/vue3';
+
+import ResetSaveButtons from '@/Components/HTML/ResetSaveButtons.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { UserForm } from '@/types';
+
+const form = useForm<UserForm>({
+    id: null!,
+    name: null!,
+    email: null!,
+    warehouse: null!,
+});
+
+const save = async (only?: Array<string>) => {
+    return new Promise<void>((resolve, reject) => {
+        form.post(route('admin.users.store'), {
+            preserveScroll: true,
+            preserveState: true,
+            only: only,
+            onSuccess: () => {
+                resolve();
+            },
+            onError: () => {
+                reject(new Error('Error, during update'));
+            },
+        });
+    });
+};
+</script>
+
+<template>
+    <Head :title="'User'" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
+            >
+                User
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <div class="grid gap-4 lg:grid-cols-1 xl:grid-cols-2">
+                            <form
+                                class="mt-6 space-y-6"
+                                @submit.prevent="save()"
+                            >
+                                <div>
+                                    <InputLabel for="name" value="Name" />
+
+                                    <TextInput
+                                        id="name"
+                                        v-model="form.name"
+                                        type="text"
+                                        class="mt-1 block w-full"
+                                        required
+                                        autocomplete="name"
+                                    />
+
+                                    <InputError
+                                        class="mt-2"
+                                        :message="form.errors.name"
+                                    />
+                                </div>
+
+                                <div>
+                                    <InputLabel for="email" value="Email" />
+
+                                    <TextInput
+                                        id="email"
+                                        v-model="form.email"
+                                        type="email"
+                                        class="mt-1 block w-full"
+                                        required
+                                        autocomplete="username"
+                                    />
+
+                                    <InputError
+                                        class="mt-2"
+                                        :message="form.errors.email"
+                                    />
+                                </div>
+
+                                <ResetSaveButtons
+                                    :processing="form.processing"
+                                    :recently-successful="
+                                        form.recentlySuccessful
+                                    "
+                                    @reset="form.reset()"
+                                />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
